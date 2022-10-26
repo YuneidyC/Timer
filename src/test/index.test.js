a8319364b4a5e26e17fd0043663374bb7a91a2ab
YuneidyC <yuneidycastillo25@gmail.com>
2022-10-26 18:27:51 +0100

    Add meta description

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,1 +1,8 @@
-test('should false', () => { expect(false).toBe(false) });
+const { checkMinutesAndSeconds } = require('../index');
+
+// test('should false', () => { expect(false).toBe(false) });
+
+test('Correct format minutes >= 0 and seconds 0 to 59', () => {
+    const rta = checkMinutesAndSeconds(-1, 0, 'Correct format minutes >= 0 and seconds 0 to 59');
+    expect(rta).toBe(false);
+});
